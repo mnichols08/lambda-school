89c2b0d9a36ea8792298898189c16c940ab37539
Mike Nichols <mikenichols410@gmail.com>
2019-07-22 20:24:08 -0400

    shorted arrow functions more

--- web-fundamentals/javascript-fundamentals/javascript-i/assignments/arrays.js
+++ web-fundamentals/javascript-fundamentals/javascript-i/assignments/arrays.js
@@ -62,7 +62,8 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 console.log(`==== Arrays.js ====`);
 console.log(`==== Challenge 1 ====`)
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-const car33 = inventory.map(car => car).filter(car => car.id === 33)[0];
+const car33 = inventory.map(
+    car => car).filter(car => car.id === 33)[0];
 console.log('Remind me what car 33 was again?',`Car 33 is a ${car33.car_year} ${car33.car_make} ${car33.car_model}` ); 
 console.log(`==== Challenge 2 ====`);
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
@@ -82,5 +83,5 @@ const oldCars = inventory.map(car => car.car_year).filter(year => year < 2000);
 console.log('Cars older than the year 2000:',oldCars.length); 
 console.log(`==== Challenge 6 ====`);
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-const BMWAndAudi = inventory.map(car => car).filter(car => car.car_make === 'BMW' || car.car_make === 'Audi');
+const BMWAndAudi = inventory.filter(car => car.car_make === 'BMW' || car.car_make === 'Audi');
 console.log('Array of Beamers and Audis:',JSON.stringify(BMWAndAudi));
--- web-fundamentals/javascript-fundamentals/javascript-i/assignments/function-conversion.js
+++ web-fundamentals/javascript-fundamentals/javascript-i/assignments/function-conversion.js
@@ -8,32 +8,32 @@ console.log(`==== Function-Conversion.js ====`);
 // };
 // myFunction();
 
-    const myFunction = () => console.log('Function was invoked');
-    myFunction();
+    const myFunction = (() => console.log('Function was invoked'))();
+    //myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
-    const anotherFunction = (param) => param;
-    anotherFunction('Example');
+    const anotherFunction = (param => param)('Example');
+   // anotherFunction('Example');
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
-    const add = (x, y) => x + y;
-    add(1,2);
+    const add = ((x, y) => x + y)(1,2);
+    // add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-    const subtract = (x, y) => x - y;
-    subtract(1,2);
+    const subtract = ((x, y) => x - y)(1,2);
+    // subtract(1,2);
 
 // Stretch
 
